f20190011c1418b1a85a0daed7f160277008e302
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-04-11 17:32:50 -0500

    APP

---
+++ CODIGO/resources/views/loja.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja de Moveis</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Loja de Móveis</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Início</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produtos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <h1 class="display-4">Bem-vindo à nossa Loja de Móveis</h1>
+            <p class="lead">Encontre os melhores móveis para sua casa aqui.</p>
+            <a href="#" class="btn btn-primary btn-lg">Ver Produtos</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://i.pinimg.com/originals/50/2b/67/502b67618a77344df6d6dfc3503e0069.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Sofá Confortável</h5>
+                        <p class="card-text">Afunde-se no luxo com nosso sofá confortável, projetado para proporcionar o máximo de conforto e estilo à sua sala de estar. Feito com materiais de alta qualidade e estofamento macio, este sofá é o lugar perfeito para relaxar após um longo dia.</p>
+                        <a href="#" class="btn btn-primary">Detalhes</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://cdn.atelierclassico.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/c/a/cadeira-com-braco-chantal-irlanda-4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Cadeira Estilosa</h5>
+                        <p class="card-text">Adicione um toque de elegância à sua casa com nossa cadeira estilosa. Com um design moderno e acabamentos de alta qualidade, esta cadeira é tanto uma peça funcional quanto uma obra de arte. Seja para uma sala de jantar sofisticada ou um canto aconchegante, esta cadeira será o destaque do seu espaço.</p>
+                        <a href="#" class="btn btn-primary">Detalhes</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://product-hub-prd.madeiramadeira.com.br/285701452/images/ae2fa4fe-633f-44aa-98bc-706102de707d2564cb49bd8de134d64dab6f1.jpeg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Mesa Moderna</h5>
+                        <p class="card-text">Transforme sua sala de jantar com nossa mesa moderna e minimalista. Com linhas limpas e uma estética contemporânea, esta mesa é perfeita para refeições em família ou para receber convidados. Feita com materiais duráveis e acabamentos elegantes, esta mesa irá adicionar estilo e funcionalidade ao seu ambiente.</p>
+                        <a href="#" class="btn btn-primary">Detalhes</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="bg-dark text-white mt-5 py-4">
+        <div class="container text-center">
+            &copy; 2024 Loja de Móveis. Interface criado pelo VILHALVA.
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ README.md
@@ -0,0 +1,40 @@
+# LOJA DE IMOVEIS COM LARAVEL
+👨‍🏫PROJETO DE INTERFACE CRIADO COM LARAVEL E BOOTSTRAP.
+
+<img src="./IMAGENS/FOTO_1.png" align="center" width="500"> <br> 
+<img src="./IMAGENS/FOTO_2.png" align="center" width="500"> <br> 
+<img src="./IMAGENS/FOTO_3.png" align="center" width="500"> <br> 
+
+## DESCRIÇÃO:
+- O projeto é um exemplo simples de um site de móveis desenvolvido com o framework Laravel. Ele apresenta uma página inicial com uma barra de navegação, um banner de boas-vindas e uma seção de produtos com cards de sofá, cadeira e mesa. Este projeto demonstra como criar e renderizar páginas usando templates Blade do Laravel, definir rotas, criar controladores e estilizar o conteúdo com CSS Bootstrap.
+
+- Este projeto serve como um ponto de partida simples para aprender os conceitos básicos do desenvolvimento web com Laravel e pode ser expandido e personalizado de acordo com suas necessidades e habilidades.
+
+## EXECUTANDO O PROJETO:
+1. **Instalando Dependências:**
+   Navegue até o diretório do projeto clonado e execute o comando abaixo para instalar todas as dependências listadas no arquivo `composer.json`:
+   ```
+   composer install
+   ```
+
+2. **Executando o Servidor Embutido:**
+   Após a instalação das dependências, inicie o servidor embutido do PHP executando o seguinte comando:
+   ```
+   php artisan serve
+   ```
+
+3. **Acessando o Site:**
+   Abra um navegador da web e navegue até o endereço `http://localhost:8000` para visualizar o site. Você será recebido com a página inicial do `Loja de Moveis`, onde poderá explorar os produtos e navegar pela barra de navegação.
+
+## NÃO SABE?
+- Entendemos que para manipular arquivos em `HTML`, `CSS` e outras linguagens relacionadas, é necessário possuir conhecimento nessas áreas. Para auxiliar nesse aprendizado, oferecemos cursos gratuitos disponíveis:
+* [CURSO DE HTML E CSS](https://github.com/VILHALVA/CURSO-DE-HTML-E-CSS)
+* [CURSO DE LARAVEL](https://github.com/VILHALVA/CURSO-DE-LARAVEL)
+* [CURSO DE BOOTSTRAP](https://github.com/VILHALVA/CURSO-DE-BOOTSTRAP)
+* [CONFIRA MAIS CURSOS](https://github.com/VILHALVA?tab=repositories&q=+topic:CURSO)
+
+## CREDITOS:
+- [PROJETO CRIADO PELO VILHALVA](https://github.com/VILHALVA)
+
+
+
